a7b5bf75a617488638e8b99c21e28f952da86446
27조 <79817453+unkwn22@users.noreply.github.com>
2021-06-20 22:29:04 +0900

    feat: 유저 정보 조회

--- src/main/java/com/example/testproject/controller/UserController.java
+++ src/main/java/com/example/testproject/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.example.testproject.controller;
 
 import com.example.testproject.dto.CreateRequestDto;
-import com.example.testproject.dto.CreateResponseDto;
+import com.example.testproject.dto.UserResponseDto;
 import com.example.testproject.entity.User;
 import com.example.testproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,9 +20,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("api/user")
-    public ResponseEntity<CreateResponseDto> createUser(@RequestBody CreateRequestDto createRequestDto){
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody CreateRequestDto createRequestDto){
         User user = userService.createUser(createRequestDto);
-        CreateResponseDto createResponseDto = new CreateResponseDto(user);
-        return ResponseEntity.ok().body(createResponseDto);
+        UserResponseDto userResponseDto = new UserResponseDto(user);
+        return ResponseEntity.ok().body(userResponseDto);
+    }
+
+    @GetMapping("api/user")
+    public ResponseEntity<List<UserResponseDto>> getAllUser(){
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 }
--- src/main/java/com/example/testproject/service/UserService.java
+++ src/main/java/com/example/testproject/service/UserService.java
@@ -1,13 +1,17 @@
 package com.example.testproject.service;
 
 import com.example.testproject.dto.CreateRequestDto;
-import com.example.testproject.dto.CreateResponseDto;
+import com.example.testproject.dto.UserResponseDto;
 import com.example.testproject.entity.User;
 import com.example.testproject.exception.BadRequestException;
 import com.example.testproject.repository.UserRepository;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.InternalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,24 @@ public class UserService {
                 .build();
 
         return userRepository.save(user);
+    }
+
+    public List<UserResponseDto> getAllUsers() {
+
+        int userCnt = (int) userRepository.count();
+
+        if(userCnt == 0){
+            throw new InternalException("등록된 회원이 없습니다.");
+        }
+
+        List<User> users = userRepository.findAll();
+        List<UserResponseDto> userList = new ArrayList<>();
+
+        for (User value : users) {
+            UserResponseDto user = new UserResponseDto(value);
+            userList.add(user);
+        }
+
+        return userList;
     }
 }
