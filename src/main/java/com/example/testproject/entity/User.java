ceb2744b149d61cd7b04d9e9f7f5671c8e6f3249
27조 <79817453+unkwn22@users.noreply.github.com>
2021-06-20 21:13:26 +0900

    유저 테이블 생성

---
+++ src/main/java/com/example/testproject/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.testproject.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+}
